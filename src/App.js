ca591374468d90cb0823ed9dd99e2310f958ee31
sam <md346@hotmail.com>
2019-06-15 12:10:03 +0200

    Building Logic 1.0

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
+
+
+// Funcion Principal
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [quote,setQuote] = useState({});
+
+// Install Axios para realizar consulta
+// Consulta a la API
+const consultarAPI = async () =>{
+  const resultado = await axios('https://got-quotes.herokuapp.com/quotes') ;
+
+  // Actualizamos el State
+  setQuote(resultado.data);
+  
 }
+// useEffect (Component Update & Component Didmount)
+  useEffect(
+    ()=>{
+      consultarAPI()
+  },[]
+);
+
+console.log(quote);
+    
+
+  return <p>Hola</p>
+}
+
+
 
 export default App;
